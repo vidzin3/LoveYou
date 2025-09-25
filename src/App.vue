bd5a8694054ad424cf908f3a9a47b882b49a622e
David <kouchdavid78@gmail.com>
2025-09-25 11:49:04 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -37,7 +37,7 @@ const yesBtn = ref(false)
 <template>
   <div class="screen">
     <img :src="yesBtn ? 'https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif' : 'https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif'" alt="">
-    <h1>{{ yesBtn ? "Slanh O teang jeang ke 😽" : "Will you be my Valentine?" }}</h1>
+    <h1>{{ yesBtn ? "Vid Slanh O teang jeang ke 😽" : "Will you be my Valentine?" }}</h1>
     <div class="group-btn" v-if="yesBtn == false">
       <button class="btn btn-yes"
        @click="yesBtn = true"
